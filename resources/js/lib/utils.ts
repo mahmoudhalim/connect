9dd354c92fa0a8a216a444e21ef0347431991099
mahmoudhalim <mahmoudahalim0@gmail.com>
2026-05-03 12:04:03 +0300

    feat: add dayjs for date formatting and relative time calculations

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -1,6 +1,9 @@
 import type { InertiaLinkProps } from '@inertiajs/react';
-import { clsx } from 'clsx';
 import type { ClassValue } from 'clsx';
+import { clsx } from 'clsx';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
@@ -9,4 +12,19 @@ export function cn(...inputs: ClassValue[]) {
 
 export function toUrl(url: NonNullable<InertiaLinkProps['href']>): string {
     return typeof url === 'string' ? url : url.url;
+}
+
+dayjs.extend(relativeTime);
+
+export function formatDate(dateString: string | Date): string {
+    const targetDate = dayjs(dateString);
+    const today = dayjs();
+
+    const diffInDays = today.diff(targetDate, 'day');
+
+    if (diffInDays > 7) {
+        return targetDate.format('MMMM D, YYYY');
+    }
+
+    return targetDate.fromNow();
 }
